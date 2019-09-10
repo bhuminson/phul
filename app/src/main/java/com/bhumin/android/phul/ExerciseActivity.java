abe76edb3560cbfb16332f093b31ec0c453c9071
Bhumin <bhumin0804@gmail.com>
2019-09-09 20:24:32 -0700

    added more functionality

--- app/src/main/java/com/bhumin/android/phul/ExerciseActivity.java
+++ app/src/main/java/com/bhumin/android/phul/ExerciseActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.SystemClock;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,16 +28,22 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ExerciseActivity extends AppCompatActivity {
     public LinearLayout current_session;
+    public LinearLayout previous_session;
     public Exercise mExercise;
     public int sets;
     public Button add_set_button;
     public Button delete_set_button;
     public TextView prev_session_label;
     public TextView subtitle;
+    public String base_key;
 
     public SharedPreferences inputs;
     public SharedPreferences.Editor editor;
@@ -45,11 +52,11 @@ public class ExerciseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("ExerciseActivity", "Activity Created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -58,15 +65,18 @@ public class ExerciseActivity extends AppCompatActivity {
         initExercise();
         initSharedPrefs();
 
-        if (inputs.getBoolean("show previous session", false)) {
-            prev_session_label.setVisibility(View.VISIBLE); //todo make this true after first session storage
+        if (inputs.getBoolean(base_key + "show previous session", false)) {
+            prev_session_label.setVisibility(View.VISIBLE);
+            saveSession();
+        } else {
+            prev_session_label.setVisibility(View.INVISIBLE);
         }
         sets = 0;
         initCurrentSession();
 
-//        if (inputs.getBoolean("saved", false)) {
-//            restoreInputs();
-//        }
+        if (inputs.getBoolean(base_key + "saved", false)) {
+            restoreInputs();
+        }
 
         checkButtons();
     }
@@ -89,7 +99,8 @@ public class ExerciseActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case (R.id.save_button):
                 Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
-//                saveSession();
+                saveInputs();
+                saveSession();
                 break;
             case (android.R.id.home):
                 saveInputs();
@@ -105,8 +116,8 @@ public class ExerciseActivity extends AppCompatActivity {
         current_session = findViewById(R.id.current_session);
         add_set_button = findViewById(R.id.add_set);
         delete_set_button = findViewById(R.id.delete_set);
+        previous_session = findViewById(R.id.previous_session);
         prev_session_label = findViewById(R.id.previous_session_label);
-        prev_session_label.setVisibility(View.GONE);
     }
 
     private void initSharedPrefs() {
@@ -123,6 +134,7 @@ public class ExerciseActivity extends AppCompatActivity {
         mExercise = mDay.exercises.get(exercise_index);
         setTitle(mExercise.name);
         subtitle.setText(mExercise.getDescription());
+        base_key = mExercise.name + mExercise.getDescription();
     }
 
     private void initChronometer() {
@@ -140,7 +152,7 @@ public class ExerciseActivity extends AppCompatActivity {
     }
 
     public void initCurrentSession() {
-        while(sets < mExercise.minSets) {
+        while (sets < mExercise.minSets) {
             addSetToSession(current_session);
         }
     }
@@ -182,7 +194,7 @@ public class ExerciseActivity extends AppCompatActivity {
         checkButtons();
     }
 
-    private void generateSetViews(LinearLayout session) {
+    private void generateSetViews(@NonNull LinearLayout session) {
         TableRow row = new TableRow(this);
         row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
         TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
@@ -225,53 +237,106 @@ public class ExerciseActivity extends AppCompatActivity {
     public void deleteSetInCurrentSession(View view) {
         current_session.removeViewAt(sets);
         sets--;
-//        weightInputs.remove(sets);
+        weightInputs.remove(sets);
+        repsInputs.remove(sets);
         checkButtons();
     }
 
+    //todo implement change units
     public void saveInputs() {
         editor = inputs.edit();
         int i = 0;
         for (EditText weightInput : weightInputs) {
-            editor.putString("weight" + i, weightInput.getText().toString());
+            String weight_input = weightInput.getText().toString();
+            editor.putString(base_key + "weight" + i, weight_input);
             i++;
         }
         int j = 0;
         for (EditText repsInput : repsInputs) {
-            editor.putString("reps" + j, repsInput.getText().toString());
+            editor.putString(base_key + "reps" + j, repsInput.getText().toString());
             j++;
         }
-        editor.putBoolean("saved", true);
-        editor.putInt("total weight inputs", i);
-        editor.putInt("total reps inputs", j);
+        editor.putBoolean(base_key + "saved", true);
+        editor.putInt(base_key + "total weight inputs", i);
+        editor.putInt(base_key + "total reps inputs", j);
+        editor.putInt(base_key + "sets", sets);
         editor.apply();
     }
 
-    public void restoreInputs() {
-        int weightEntries = inputs.getInt("total weight inputs", 0);
-        int repsEntries = inputs.getInt("total reps inputs", 0);
+    private void restoreRows() {
+        int weightEntries = inputs.getInt(base_key + "total weight inputs", mExercise.minSets);
+        int repsEntries = inputs.getInt(base_key + "total reps inputs", mExercise.minSets);
         while (sets - weightEntries != 0) {
             addSetToSession(current_session);
         }
         while (sets - repsEntries != 0) {
             addSetToSession(current_session);
         }
+    }
+
+    public void restoreInputs() {
+        restoreRows();
+
         int i = 0;
         for (EditText weightInput : weightInputs) {
-            weightInput.setText(inputs.getString("weight" + i, ""));
+            weightInput.setText(inputs.getString(base_key + "weight" + i, ""));
             i++;
         }
         int j = 0;
         for (EditText repsInput : repsInputs) {
-            repsInput.setText(inputs.getString("reps" + j, ""));
+            repsInput.setText(inputs.getString(base_key + "reps" + j, ""));
             j++;
         }
     }
 
-//    private void saveSession() {
-//        if(prev_session_label.getVisibility() == View.GONE) {
-//            prev_session_label.setVisibility(View.VISIBLE);
-//        }
-//
-//    }
+    private void saveSession() {
+        Format formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        String string = formatter.format(Calendar.getInstance().getTime());
+        TextView date = new TextView(this);
+        date.setText(string);
+        date.setPadding(20, 0, 0, 0);
+        previous_session.addView(date, 1);
+        int savedSets = inputs.getInt(base_key + "sets", 0);
+
+        for (int i = 0; i < savedSets; i++) {
+            TableRow row = new TableRow(this);
+            row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+            TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+            TextView set_label = new TextView(this);
+            set_label.setText(getString(R.string.dynamic_string_label, i + 1));
+            params.setMargins(20, 0, 0, 0);
+            set_label.setLayoutParams(params);
+
+            String weight = inputs.getString(base_key + "weight" + i, "0");
+            TextView weight_input = new TextView(this);
+            weight_input.setText(weight);
+            weight_input.setGravity(Gravity.END);
+            params.setMargins(50, 0, 0, 0);
+            weight_input.setLayoutParams(params);
+
+            TextView x = new TextView(this);
+            x.setText("x");
+            x.setLayoutParams(params);
+
+            String reps = inputs.getString(base_key + "reps" + i, "0");
+            TextView reps_input = new TextView(this);
+            reps_input.setText(reps);
+            reps_input.setGravity(Gravity.END);
+            reps_input.setLayoutParams(params);
+
+            row.addView(set_label);
+            row.addView(weight_input);
+            row.addView(x);
+            row.addView(reps_input);
+            previous_session.addView(row, i + 2);
+        }
+        while (previous_session.getChildAt(savedSets + 2) != null) {
+            previous_session.removeViewAt(savedSets + 2);
+        }
+        editor = inputs.edit();
+        editor.putBoolean(base_key + "show previous session", true);
+        prev_session_label.setVisibility(View.VISIBLE);
+        editor.apply();
+    }
 }
